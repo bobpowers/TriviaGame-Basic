2cad9ccee94dc23b2d4f061fe440a2a266c1cb8e
Robert Powers <bobpowers@me.com>
2018-04-02 13:20:59 -0500

    clean/adjust JS file. small changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,9 @@
 
 
 var countdown = 40;
-var factChecker;
 var rightAnswers = 0;
 var wrongAnswers = 0;
 var intervalProcess;
-var resultsWindow;
 
 // Figure out if you can combine these 3 countdown functions
 var countdownDecrement = function(){
@@ -25,7 +23,7 @@ var decrement = function(){
       }
 }
 
-factChecker = function(){
+var factChecker = function(){
 	$('input:radio').each(function(){
   		if($(this).is(':checked')) {
   			if ($(this).val() === "true") {
@@ -46,7 +44,7 @@ $("#finished").on("click", function(){
 	factChecker();
 });
 
-resultsWindow = function(){
+var resultsWindow = function(){
 	$(".results").show();
 	$("#rightAnswers").text("Right Answers: " + rightAnswers);
 	$("#wrongAnswers").text("Wrong Answers: " + wrongAnswers);
@@ -74,59 +72,3 @@ $(document).ready(function() {
 
 	});
 });
-
-
-
-
-
-
-
-
-// STUFF THAT DIDNT QUITE WORK:
-
-
-// FAILED ATTEMPT TO CALL OUTSIDE VAR IN JQUERY SELECTOR- HAD SOME PROGRESS
-// 	factChecker = function(){
-// 		for (var i = 0; i < 10; i++) {
-// 			var radioValue = $("input[name=" 'answerContainer[i]' "]:checked").val();
-// 			if (radioValue === "false") {
-// 				wrongAnswers++;
-// 				console.log(wrongAnswers);
-// 			}
-// 			else if (radioValue === "true") {
-// 				rightAnswers++;
-// 				console.log(rightAnswers)
-// 			}
-// 			console.log(wrongAnswers + " WrOutFor");
-// 			console.log(rightAnswers + " RiOutFor");
-
-// 	}
-// }
-
-	// $("input[type='radio']").click(function(){
-            // var radioValue = $("input[name='Question1']:checked").val();
- //            if (radioValue === "false"){
- //            	wrongAnswers++;
- //            	console.log(wrongAnswers);
- //            }
- //            else if (radioValue === "true"){
- //            	rightAnswers++;
- //            	console.log(rightAnswers);
- //            }
-	// });
-// }
-
-
-// factChecker = function(){
-// 	for (var i = 0; i < 10; i++) {
-		// userAnswer = document.querySelector('input[name=Question'+i+']:checked')||{}).value;
-// 		if (userAnswer === "true"){
-// 			rightAnswers++
-// 		}
-// 		else if (userAnswer === "false"){
-// 			wrongAnswers++
-// 		}
-
-
-
-
